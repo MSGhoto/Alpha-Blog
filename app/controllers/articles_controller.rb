9e0c2e4426b8614428dd272410616783bbc23faf
m.suleman <muhammad.suleman@teknuk.com>
2022-12-07 14:28:05 +0500

    showing list on listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         
     end
+
+    def index
+        @article = Article.all
+    end
+    
     
     
 end
