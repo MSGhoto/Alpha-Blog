d8c6b005e6c2b860ddea4b948fea48344b67957e
MSGhoto <sulemanghoto630729@gmail.com>
2022-12-12 15:41:55 +0500

    edit and update action completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,21 +10,39 @@ class ArticlesController < ApplicationController
     end
 
     def new
-        
+        @article = Article.new
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     
     def create
         @article = Article.new(params.require(:article).permit(:title,:description))
-        @article.save
-        redirect_to @article
-       # if @article.save
-       #   flash[:success] = "Object successfully created"
-       #   redirect_to @article
-      #  else
-       #   flash[:error] = "Something went wrong"
-      #    render 'new'
-      #  end
+
+
+        if @article.save
+            flash[:success] = "Article successfully created"
+            redirect_to @article
+        else
+            flash[:error] = "Something went wrong. Coudn't creat Article"
+            render 'new'
+        end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:success] = "Article was Updated Successfully"
+            redirect_to @article
+        else
+            flash[:error] = "Something went wrong. couldn't update Article"
+            render 'edit'
+        end
+        
+    end
+    
     
     
     
