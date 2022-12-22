273ab025f47ff65b3577647f78eb42a92941d49c
MSGhoto <sulemanghoto630729@gmail.com>
2022-12-22 19:53:05 +0500

    home page layout comeplet and article index almost finish

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     
     validates :title, presence: true, length:{minimum:5,maximum:100}
-    validates :description, presence: true, length:{minimum:5,maximum:500}
+    validates :description, presence: true, length:{minimum:5,maximum:1000}
     
 end
