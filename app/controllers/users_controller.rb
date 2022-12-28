8d54b3bd6bfacf490c1d26d42263e4addba79876
MSGhoto <sulemanghoto630729@gmail.com>
2022-12-28 20:00:35 +0500

    added user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @article = @user.Articles
     end
+
+    def index
+        @users = User.all
+    end
+    
     
     def new
         @user = User.new
